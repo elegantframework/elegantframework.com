755f71a5f76164044d7c2ac64c8f697fe35f4ffe
Brandon Owens <brandono@vitamins2you.com>
2023-03-16 23:17:18 -0400

    SSR Fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+import Layout from "@theme/Layout";
+import React from "react";
+
 export default function Home(): JSX.Element {
   //
   // Temporarily redirect to the docs until we find a workaround for Docusaurus' '/' slug handling.
@@ -5,5 +8,8 @@ export default function Home(): JSX.Element {
   if (typeof window !== "undefined") {
     window.location.href = '/docs';
     return null;
+  }
+  else{
+    return(<Layout>{null}</Layout>);
   }
 }
