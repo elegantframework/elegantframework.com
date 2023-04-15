54510d41f05203e633d3681ba87ba65517b3875c
Brandon Owens <brandono@vitamins2you.com>
2023-04-15 09:08:39 -0400

    Completed the deployment doc

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 ~~~~~~> Working 
 
-- finish the deployment doc
+- add configuring your domain to the deployment doc
 
 - update the "creating a page doc".. It is outdated and based on docusaurus
     - should probably have a creating a new doc, page, and blog post guide.
--- src/components/Docs/DeploymentSteps.tsx
+++ src/components/Docs/DeploymentSteps.tsx
@@ -1,5 +1,8 @@
 import { Steps } from '@/components/Steps';
 import vercel_dashboard from '@/img/docs/Vercel.com-Dashboard.png'
+import vercel_project from '@/img/docs/Vercel.com-CreateNewProject.png';
+import vercel_configuration from '@/img/docs/Vercel.com-ConfigureProject.png';
+import vercel_deployment_success from '@/img/docs/Vercel.com-DeploymentSuccess.png';
 
 /**
  * Our deployment steps for the deployment guide
@@ -29,8 +32,37 @@ const DeploymentSteps = () => {
               </p>
             ),
             image: {
-                title: "",
-                src: ""
+                title: "Select your Elegant project to deploy to Vercel.com",
+                src: vercel_project.src
+            }
+        },
+        {
+            title: 'Verify the configuration settings for your application',
+            body: () => (
+              <p>
+                You can skip setting environment variables for now. We will configure the environment variables in a later step.
+              </p>
+            ),
+            image: {
+                title: "Verify the Vercel project settings",
+                src: vercel_configuration.src
+            }
+        },
+        {
+            title: 'Click Deploy',
+            body: () => (
+              <>
+                <p>
+                    Wait for the build and deployment steps to complete.
+                </p>
+                <p>
+                    If you into any issues during deployment, you can inspect the build console output to troubleshoot and resolve any deployment errors.
+                </p>
+              </>
+            ),
+            image: {
+                title: "Click deploy to launch your Elegant application.s",
+                src: vercel_deployment_success.src
             }
         }
     ];
