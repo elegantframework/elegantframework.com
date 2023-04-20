dac5e288e3c96fc960536f13fa264b35fc0d8737
Brandon Owens <brandono@vitamins2you.com>
2023-04-19 22:10:21 -0400

    Work in progress

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -42,20 +42,6 @@ const Hero = () => {
           </li>
         </ul>
       </div>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 mt-20 sm:mt-24 lg:mt-32 lg:grid lg:gap-8 lg:grid-cols-12 lg:items-center">
-        <div className="relative row-start-1 col-start-6 xl:col-start-7 col-span-7 xl:col-span-6">
-          <div className="-mx-4 sm:mx-0">
-            Right side
-          </div>
-        </div>
-        <div className="relative row-start-1 col-start-1 col-span-5 xl:col-span-6 -mt-10">
-          <div className="h-[24.25rem] max-w-xl mx-auto lg:max-w-none flex items-center justify-center">
-            <div className="w-full flex-none">
-              Left side
-            </div>
-          </div>
-        </div>
-      </div>
     </section>
   )
 }
