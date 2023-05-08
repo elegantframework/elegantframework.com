94a303d8dc6c7a1797ba0981b5bed80afeda0173
Brandon Owens <brandono@vitamins2you.com>
2023-05-08 14:39:36 -0400

    Update documentation to use the new SEO component
    Updated the documentation layout to use the new seo component to set the page title.
    
    Also updated the meta title to be more descript and SEO friendly for all the current document pages,

--- src/layouts/DocumentationLayout.js
+++ src/layouts/DocumentationLayout.js
@@ -1,11 +1,14 @@
-import { SidebarLayout } from '@/layouts/SidebarLayout'
-import { Title } from '@/components/Meta'
-import { documentationNav } from '@/navs/documentation'
+import { SidebarLayout } from '@/layouts/SidebarLayout';
+import { Title } from '@/components/Meta';
+import { documentationNav } from '@/navs/documentation';
+import Seo from "@/components/core/Seo/Seo";
 
 export function DocumentationLayout(props) {
   return (
     <>
-      <Title>{props.layoutProps.meta.metaTitle || props.layoutProps.meta.title}</Title>
+      <Seo 
+        title={props.layoutProps.meta.metaTitle || props.layoutProps.meta.title}
+      />
       <SidebarLayout nav={documentationNav} {...props} />
     </>
   )
