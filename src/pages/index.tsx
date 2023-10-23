fffccb2319d396a4d700299ccfa3684ec4424364
Brandon Owens <brandono@vitamins2you.com>
2023-10-22 20:22:43 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -303,9 +303,9 @@ const Header = ({
       <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Latest Updates</h2>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">The latest Elegant news</h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
-            The latest updates and news straight from the Elegant Framework blog.
+            Updates and news, straight from the Elegant Framework blog.
           </p>
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
